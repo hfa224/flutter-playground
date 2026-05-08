9663ef7b1df4c5a75e0a755135bac8c72244d201
hfa224 <helenffionadams@gmail.com>
2026-05-08 11:27:02 +0100

    Initial commit of flutter app based on birdle game tutorial

---
+++ README.md
@@ -0,0 +1,13 @@
+# birdle
+
+A new Flutter project. Created to follow the [flutter tutorial](https://docs.flutter.dev/learn/pathway/tutorial).
+
+Run with:
+
+```flutter run```
+
+Build with:
+
+```
+flutter build web
+```
---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'game.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Riskaware years!',
+              style: TextStyle(
+                fontFamily: 'Bonbon',
+                fontWeight: FontWeight.normal,
+                fontStyle: FontStyle.normal,
+                fontSize: 40.0,
+              ),
+            ),
+          ),
+        ),
+        body: Center(child: GamePage()),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  // This object is part of the game.dart file.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: SizedBox(
+          width: 360,
+          child: Column(
+            spacing: 5.0,
+            children: [
+              for (final guess in _game.guesses)
+                Row(
+                  spacing: 5.0,
+                  children: [
+                    for (final record in guess)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 2.5,
+                          vertical: 2.5,
+                        ),
+                        child: Tile(record.char, record.type),
+                      ),
+                  ],
+                ),
+              GuessInput(
+                onSubmitGuess: (guess) {
+                  setState(() {
+                    // NEW
+                    print(guess);
+                    _game.guess(guess);
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: const Image(image: AssetImage('assets/pngs/tamagotchi.png')),
+      ),
+    );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text.trim());
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (_) {
+                _onSubmit();
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.arrow_circle_up),
+          onPressed: () {
+            _onSubmit();
+          },
+        ),
+      ],
+    );
+  }
+}
